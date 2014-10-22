ad644a9da5e0c25e8259692e11dd9b37972602e0
Umayr Shahid <umayrr@hotmail.co.uk>
2014-10-23 00:00:01 +0500

    core: main logic

---
+++ olx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using olx.Models;
+
+namespace olx
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            foreach (var category in Ad.Categories)
+            {
+                DateTime startTime = DateTime.Now;
+                var records = (new Fetch()).Begin(category);
+                if (!records.HasValue)
+                    continue;
+                Console.WriteLine("Fetched {0} records in {1} seconds in {2}. {3}",
+                    records, (DateTime.Now - startTime).TotalSeconds, category, Environment.NewLine);
+            }
+
+            Console.WriteLine("Yay! You just cloned all of OLX's data. \nPress any key to exit.");
+
+        }
+
+
+
+    }
+}
