ea3128b26445617a590897d82b0940b2a2423498
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-13 00:00:01 +0500

    api: routes and web api configuration

---
+++ olx.api/App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace olx.api
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute(
+                name: "Default",
+                url: "index.html"
+            );
+        }
+    }
+}
---
+++ olx.api/App_Start/WebApiConfig.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace olx.api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+         
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}"
+            );
+            config.Routes.MapHttpRoute(
+                name: "All Ads",
+                routeTemplate: "api/{controller}/all",
+                defaults: new { action = "All" }
+            );
+            config.Routes.MapHttpRoute(
+                name: "Images of an Ad",
+                routeTemplate: "api/{controller}/images/{uniqueId}",
+                defaults: new { action = "Images" }
+            );
+            config.Routes.MapHttpRoute(
+                name: "Ad Images",
+                routeTemplate: "api/{controller}/images",
+                defaults: new { action = "Images" }
+            );
+            config.Routes.MapHttpRoute(
+                name: "Ad Get",
+                routeTemplate: "api/{controller}/get",
+                defaults: new { action = "Get" }
+            );
+            config.Routes.MapHttpRoute(
+                name: "Ad Count",
+                routeTemplate: "api/{controller}/count",
+                defaults: new { action = "Count" }
+            );
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            config.EnableSystemDiagnosticsTracing();
+        }
+    }
+}
