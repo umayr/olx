0748ecc7dbd54bb29ea91d948844091aafe3f7d1
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-17 00:00:01 +0500

    api: global configs

---
+++ olx.api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="olx.api.WebApiApplication" Language="C#" %>
---
+++ olx.api/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace olx.api
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
